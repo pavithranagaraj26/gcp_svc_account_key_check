2d764671bde814959f1939375e24677aca31534a
im.pavithranagaraj <42617512+pavithranagaraj26@users.noreply.github.com>
2021-07-26 17:04:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,74 @@
 import os
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from jinja2 import Environment, FileSystemLoader
+# from email.mime.text import MIMEText
+# from email.mime.multipart import MIMEMultipart
+# from jinja2 import Environment, FileSystemLoader
 import base64
 import json
 import logging
 import smtplib
-import requests
-from datetime import datetime
+import datetime
+from pprint import pprint
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
 
-# opens the log file
-with open('logs.json') as json_file:
-    data_json = json.load(json_file)
-#print(type(data_json))
-# get the principleEmail
-for data in data_json:
-    print("--------------------------------------------------------------")
-    principalEmail=data['protoPayload']['authenticationInfo']['principalEmail']
-    #print("Principal Email:" , principalEmail)
-    # get the project id
-    project_id=data['resource']['labels']['project_id']
-    #print("project id:", project_id)
-    # get the email_id of the user
-    email_id=data['resource']['labels']['email_id']
-    #print("email id:", email_id)
-    # get the details user email
-    details = data['protoPayload']['authenticationInfo']['principalSubject']
-    #print("details:",details)
-    details= details.split(":")[1]
-    print("created_by:",details)
-    domain = details.split('@')[1]
-    print("domain", domain)
-    try:
-        create_date= data['protoPayload']['response']['valid_after_time']['seconds']
-        #print("create_date:",create_date)
-        expiry_date = data['protoPayload']['response']['valid_before_time']['seconds']
-        #print("expiry_date:", expiry_date)
-        create_date_format = datetime.fromtimestamp(create_date).strftime('%Y-%m-%d %I:%M:%S %p')
-        create_date_format = datetime.strptime(create_date_format,'%Y-%m-%d %I:%M:%S %p' )
-        print("create date:", create_date_format )
-        expiry_date_format = datetime.fromtimestamp(expiry_date).strftime('%Y-%m-%d %I:%M:%S %p')
-        expiry_date_format = datetime.strptime(expiry_date_format,'%Y-%m-%d %I:%M:%S %p' )
-        print("expiry_date:", expiry_date_format)
-        check = 90
-        now = datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')
-        now = datetime.strptime(now,'%Y-%m-%d %I:%M:%S %p' )
-        #print("now:", now)
-        #print(type(now))
-        delta = (expiry_date_format - create_date_format).days
-        #print(delta)
-        delta1 = (now - create_date_format).days
-        #print(delta1)
+with open('org_level_check.json') as json_file:
+    data = json.load(json_file)
+principalEmail=data['protoPayload']['authenticationInfo']['principalEmail']
+#print(principalEmail)
+project_id=data['resource']['labels']['project_id']
+#print(project_id)
+details = data['protoPayload']['serviceData']['policyDelta']['bindingDeltas']
+#print(details)
+user_details = [i['member'] for i in details]
+#print(user_details)
+email = "\r\n".join(user_details)
+#print(email)
+member_details = email.split(':')[0]
+member_details_email = email.split(':')[1]
+if member_details == 'serviceAccount':
+    print("It is a service account")
+    print (member_details_email)
+# def list_key(service_account_email):
 
-        if delta > 90:
-            print("level1 check")
-            print("reset service account key ")
-        elif delta1 > 90:
-            print("level2 check")
-            print("reset service account key")
-        else:
-            print("service account key")
-    except:
-        print("cannot find service account key so skip")
+credentials = GoogleCredentials.get_application_default()
+service = discovery.build('iam', 'v1', credentials=credentials)
+key = service.projects().serviceAccounts().keys().list(name='projects/-/serviceAccounts/' + member_details_email").execute()
+#key = service.projects().serviceAccounts().keys().list(name='projects/-/serviceAccounts/' + member_details_email, keyTypes="USER_MANAGED").execute()
+#key = service.projects().serviceAccounts().keys().list(name='projects/-/serviceAccounts/' + member_details_email, keyTypes="SYSTEM_MANAGED").execute()
+#pprint(key)
 
+for ukey in key["keys"]:
+    keyname = ukey['name'].split("/")[-1]
+    validFrom = ukey['validAfterTime']
+    validUntil = ukey['validBeforeTime']
+    korigin = ukey['keyOrigin']
+    ktype = ukey['keyType']
+    print(f"Key_name:{keyname},\nValid_from:{validFrom},\nValid_until:{validUntil},\nKey_origin:{korigin},\nKey_type:{ktype}")
 
-    for project in project_id.split(","):
-        for svc_email_id in email_id.split(","):
-            print(project)
-            print(svc_email_id)
+    timestringa = ukey['validAfterTime']
+    timestringb = ukey['validBeforeTime'] 
+    # Create datetime objects (now-createdate)
+    d0 = datetime.datetime.strptime(timestringa, "%Y-%m-%dT%H:%M:%SZ")
+    d1 = datetime.datetime.now() # Current time
+
+    # Calculate timedelta (now-createdate)
+    dt = d1 - d0
+
+    # Create datetime objects (now-createdate)
+    d2 = datetime.datetime.strptime(timestringb, "%Y-%m-%dT%H:%M:%SZ")
+    #calculate the rotation period (validuntil - validfrom)
+    de = d2 - d0
+    rotation_age = de.days
+    print(f'{member_details_email} roation_age:',rotation_age )
+    passed_days = dt.days
+    passed_seconds = dt.days # remaining seconds
+    passed_microseconds = dt.microseconds # remaining microseconds
+    total_seconds = dt.total_seconds()
+    print(f'{member_details_email} keyage:',passed_days)
     
-
-        
+    if rotation_age > 90 :
+        print("change the roation period to 90 days")
+        if passed_days > 90: 
+            print("compliant")       
+    else:
+        pass
